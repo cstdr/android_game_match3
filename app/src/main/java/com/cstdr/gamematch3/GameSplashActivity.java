4a92dfef7790ae648fd52a2e40ff30960856e90d
Roy <cstdingran@gmail.com>
2023-02-21 17:04:54 +0800

    init 游戏大概功能和技术点。

---
+++ app/src/main/java/com/cstdr/gamematch3/GameSplashActivity.java
@@ -0,0 +1,14 @@
+package com.cstdr.gamematch3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_splash);
+    }
+}
